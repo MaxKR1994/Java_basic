c1bb182e12bc5cdb13c93fdd6c2ab842d5b9dfab
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-04-27 22:12:47 +0300

    Initial commit

---
+++ src/Phone.java
@@ -0,0 +1,81 @@
+public class Phone {
+    private String color;
+    private String operatingSystem;
+    private Integer batteryCapacity;
+    private Integer screenSize;
+    private Integer memory;
+
+    public static String stock;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public Integer getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(Integer batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public Integer getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(Integer screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public Integer getMemory() {
+        return memory;
+    }
+
+    public void setMemory(Integer memory) {
+        this.memory = memory;
+    }
+
+    public Phone(String color, String operatingSystem, Integer batteryCapacity, Integer screenSize, Integer memory) {
+        this.color = color;
+        this.operatingSystem = operatingSystem;
+        this.batteryCapacity = batteryCapacity;
+        this.screenSize = screenSize;
+        this.memory = memory;
+    }
+
+    public Phone() {
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "color='" + color + '\'' +
+                ", operatingSystem='" + operatingSystem + '\'' +
+                ", batteryCapacity=" + batteryCapacity +
+                ", screenSize=" + screenSize +
+                ", memory=" + memory +
+                '}';
+    }
+
+    public String toString(String color, String operatingSystem, Integer batteryCapacity, Integer screenSize, Integer memory) {
+        return "Phone{" +
+                "color='" + color + '\'' +
+                ", operatingSystem='" + operatingSystem + '\'' +
+                ", batteryCapacity=" + batteryCapacity +
+                ", screenSize=" + screenSize +
+                ", memory=" + memory +
+                '}';
+    }
+}
